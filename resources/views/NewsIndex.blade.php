6c711d5713005c28e6824a628298e2372d14a30e
mantowar4 <34520802+mantowar4@users.noreply.github.com>
2020-07-14 11:55:00 +0600

    news blade

--- resources/views/NewsIndex.blade.php
+++ resources/views/NewsIndex.blade.php
@@ -1,35 +1,5 @@
 @extends('layouts.layout', ['title'=>'Новости'])
 
 @section('content')
-    <div class="container">
-        @if(isset($_GET['search']))
-            @if(count($news)>0)
-                <h2>Результаты поиска по запросу "<?=$_GET['search']?>" </h2>
-                <p class="lead">Всего найдено {{count($news)}} записей</p>
-            @else
-                <h2>По запросу "<?=$_GET['search']?>" ничего не найдено.</h2>
-                <a href="{{route('news.index',app()->getLocale())}}" class="btn btn-outline-success my-2 my-sm-0">Отобразить все новости.</a>
-            @endif
-        @endif
-
-        <div class="card-group">
-            @foreach($news as $new)
-                <div class="card-deck col-lg-6">
-                    <div class="card">
-                        <img class="card-img-top" src="{{ $new->news_img ?? asset('img/default-img.jpg')}}" alt="Card image cap">
-                        <div class="card-body">
-                            <h5 class="card-title">{{$new->news_short_title}}</h5>
-                            <a href="{{ route('articles.show', ['id' => $new->news_id,app()->getLocale()]) }}" class="btn btn-info my-2 my-sm-0">Подробнее</a>
-                        </div>
-                        <div class="card-footer">
-                            <small class="text-muted">Created {{$new->created_at}}</small>
-                        </div>
-                    </div>
-                </div>
-            @endforeach
-        </div>
-        @if(!isset($_GET['search']))
-            <div class="links">{{ $news->links() }}</div>
-        @endif
-    </div>
+    
 @endsection
