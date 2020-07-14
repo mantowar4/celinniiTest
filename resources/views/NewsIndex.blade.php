ff1dd116ad87e918ba4e66bf4855210a24016398
mantowar4 <34520802+mantowar4@users.noreply.github.com>
2020-07-14 11:51:30 +0600

    news blade

--- resources/views/NewsIndex.blade.php
+++ resources/views/NewsIndex.blade.php
@@ -3,96 +3,33 @@
 @section('content')
     <div class="container">
         @if(isset($_GET['search']))
-            <!---
-                @if(count($news)>0)
+            @if(count($news)>0)
                 <h2>Результаты поиска по запросу "<?=$_GET['search']?>" </h2>
-                <p class="lead">Всего найдено {{count($news)+count($engins)+count($posts)}} записей</p>
+                <p class="lead">Всего найдено {{count($news)}} записей</p>
             @else
                 <h2>По запросу "<?=$_GET['search']?>" ничего не найдено.</h2>
-                <a href="{{route('news.index',app()->getLocale())}}" class="btn btn-outline-success my-2 my-sm-0">Отобразить все
-                    новости.</a>
-                <a href="{{route('news.index',app()->getLocale())}}" class="btn btn-outline-success my-2 my-sm-0">Отобразить все
-                    публикации.</a>
-                <a href="{{route('news.index',app()->getLocale())}}" class="btn btn-outline-success my-2 my-sm-0">Отобразить все
-                    разработки.</a>
+                <a href="{{route('news.index',app()->getLocale())}}" class="btn btn-outline-success my-2 my-sm-0">Отобразить все новости.</a>
             @endif
-            <div class="card-group">
-                @foreach($news as $new)
-                    <div class="card-deck col-lg-6">
-                        <div class="card">
-                            <img class="card-img-top" src="{{ $new->news_img ?? asset('img/default-img.jpg')}}"
-                                 alt="Card image cap">
-                            <div class="card-body">
-                                <h5 class="card-title">{{$new->news_short_title}}</h5>
-                                <a href="{{ route('news.show', ['id' => $new->news_id]) }}"
-                                   class="btn btn-info my-2 my-sm-0">Подробнее</a>
-                            </div>
-                            <div class="card-footer">
-                                <small class="text-muted">Created {{$new->created_at}}</small>
-                            </div>
-                        </div>
-                    </div>
-                @endforeach
-                @if(!empty($engins))
-                    @foreach($engins as $engin)
-                        <div class="card-deck col-lg-6">
-                            <div class="card">
-                                <img class="card-img-top" src="{{ $engin->engin_img ?? asset('img/default-img.jpg')}}"
-                                     alt="Card image cap">
-                                <div class="card-body">
-                                    <h5 class="card-title">{{$engin->engin_short_title}}</h5>
-                                    <a href="{{ route('news.show', ['id' => $engin->engin_id,app()->getLocale()]) }}"
-                                       class="btn btn-info my-2 my-sm-0">Подробнее</a>
-                                </div>
-                                <div class="card-footer">
-                                    <small class="text-muted">Created {{$engin->created_at}}</small>
-                                </div>
-                            </div>
-                        </div>
-                    @endforeach
-                @endif
-                @if(!empty($posts))
-                    @foreach($posts as $post)
-                        <div class="card-deck col-lg-6">
-                            <div class="card">
-                                <img class="card-img-top" src="{{ $post->post_img ?? asset('img/default-img.jpg')}}"
-                                     alt="Card image cap">
-                                <div class="card-body">
-                                    <h5 class="card-title">{{$post->post_short_title}}</h5>
-                                    <a href="{{ route('news.show', ['id' => $post->post_id,app()->getLocale()]) }}"
-                                       class="btn btn-info my-2 my-sm-0">Подробнее</a>
-                                </div>
-                                <div class="card-footer">
-                                    <small class="text-muted">Created {{$post->created_at}}</small>
-                                </div>
-                            </div>
-                        </div>
-                    @endforeach
-                @endif
-            </div>
-            --->
-        @else
-            <div class="card-group">
-                @foreach($news as $new)
-                    <div class="card-deck col-lg-6">
-                        <div class="card">
-                            <img class="card-img-top" src="{{ $new->news_img ?? asset('img/default-img.jpg')}}"
-                                 alt="Card image cap">
-                            <div class="card-body">
-                                <h5 class="card-title">{{$new->news_short_title}}</h5>
-                                <a href="{{ route('news.show', ['id' => $new->news_id,app()->getLocale()]) }}"
-                                   class="btn btn-info my-2 my-sm-0">Подробнее</a>
-                            </div>
-                            <div class="card-footer">
-                                <small class="text-muted">Created {{$new->created_at}}</small>
-                            </div>
-                        </div>
-                    </div>
-                @endforeach
-            </div>
         @endif
+
+        <div class="card-group">
+            @foreach($news as $new)
+                <div class="card-deck col-lg-6">
+                    <div class="card">
+                        <img class="card-img-top" src="{{ $new->news_img ?? asset('img/default-img.jpg')}}" alt="Card image cap">
+                        <div class="card-body">
+                            <h5 class="card-title">{{$new->news_short_title}}</h5>
+                            <a href="{{ route('articles.show', ['id' => $new->news_id,app()->getLocale()]) }}" class="btn btn-info my-2 my-sm-0">Подробнее</a>
+                        </div>
+                        <div class="card-footer">
+                            <small class="text-muted">Created {{$new->created_at}}</small>
+                        </div>
+                    </div>
+                </div>
+            @endforeach
+        </div>
         @if(!isset($_GET['search']))
-            <div class="pagination container">{{ $news->links() }}</div>
+            <div class="links">{{ $news->links() }}</div>
         @endif
     </div>
 @endsection
