24ed018ee320a77c77544e084a7b971bf90206d2
mantowar4 <34520802+mantowar4@users.noreply.github.com>
2020-07-30 15:41:47 +0600

    30,07,2020
    яндекс метрика

--- resources/views/layouts/layout.blade.php
+++ resources/views/layouts/layout.blade.php
@@ -15,6 +15,22 @@
     <link rel="stylesheet" href="{{asset('remixicon/fonts/remixicon.css')}}">
 </head>
 <body>
+
+<!-- Yandex.Metrika counter -->
+<script type="text/javascript" >
+    (function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+        m[i].l=1*new Date();k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
+    (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
+
+    ym(66012892, "init", {
+        clickmap:true,
+        trackLinks:true,
+        accurateTrackBounce:true
+    });
+</script>
+<noscript><div><img src="https://mc.yandex.ru/watch/66012892" style="position:absolute; left:-9999px;" alt="" /></div></noscript>
+<!-- /Yandex.Metrika counter -->
+
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
 <nav class="navbar navbar-expand-sm navbar-light bg-light">
     <div class="container">
